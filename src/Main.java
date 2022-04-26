9e8726793bd34a6649a2520572b76270bb1ac5f0
tolgaerbas1 <whocanfly01@gmail.com>
2022-04-26 23:57:40 +0300

    linked list operations

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import kotlinversion.Node;
+
 public class Main {
     public static void main(String[] args) {
         // Example
@@ -5,6 +7,13 @@ public class Main {
         node.appendToEnd(5);
         node.appendToEnd(6);
         node.appendToEnd(7);
+        node.appendToEnd(8);
+        node.appendToEnd(9);
         // End of Example
+        node.printNodes();
+        System.out.println("Length is : "+node.length(node));
+        System.out.println("Sum is : "+node.sumOfNodes());
+        node.deleteNode(node,7);
+        node.printNodes();
     }
 }
--- src/kotlinversion/Node.kt
+++ src/kotlinversion/Node.kt
@@ -2,39 +2,66 @@ package kotlinversion
 
 class Node(var data: Int) {
 
-    var following: Node? = null
+    var currentNode : Node? = null
+    var nextNode: Node? = null
 
     fun appendToEnd(data: Int) {
         val end = Node(data)
         var n: Node? = this
-        while (n!!.following != null) {
-            n = n.following
+        while (n!!.nextNode != null) {
+            n = n.nextNode
         }
-        n.following = end
+        n.nextNode = end
     }
 
     // TODO:: Implement to return the length of the SinglyLinkedList
     // For example:: --> 5 --> 6 --> 7 --> 3 --> .
-    fun printNodes() {}
+    fun printNodes() {
+        currentNode = this
+        while(currentNode != null){
+            println("Node : ${currentNode!!.data}")
+            currentNode = currentNode!!.nextNode
+        }
+    }
 
     // TODO:: Implement to return the length of the SinglyLinkedList
     fun length(h: Node?): Int {
-        return 0
+        currentNode = h
+        var count : Int = 0
+        while(currentNode != null){
+            count++
+            currentNode = currentNode!!.nextNode
+        }
+        return count
     }
 
     // TODO:: Implement to return the sum of the Nodes
     fun sumOfNodes(): Int {
-        return 0
+        currentNode = this
+        var sum : Int = 0
+        while(currentNode != null){
+            sum += currentNode!!.data
+            currentNode = currentNode!!.nextNode
+        }
+        return sum
     }
 
     fun deleteNode(head: Node, data: Int): Node? {
         val n: Node = head
 
         if(n.data == data) {
-            return head.following
+            return head.nextNode
+        }
+        var prev = head
+        var current = head
+        var next = head.nextNode
+
+        while (next!!.data != data) {
+            prev = next
+            next = next.nextNode
         }
         // TODO:: Implement the proper loop in order to remove given data
-
+        prev.nextNode = next.nextNode
         //
         return head
     }
